78186b201ee2122294c91bc43b66e50e52772269
Nicoogf <nicolasgfalabella@gmail.com>
2023-11-29 15:08:28 -0300

    Portfolio Personal

---
+++ README.md
@@ -0,0 +1,36 @@
+# Nicolas Falabella - Portafolio
+
+¡Bienvenido a mi repositorio de mi Portfolio Profesional! 👋
+
+## Acerca de mí
+
+¡Hola! Soy Nicolas, un apasionado desarrollador web con experiencia en la creación de soluciones creativas y eficientes. Después de obtener mi título en Administración, decidí seguir mi verdadera pasión por la programación y sumergirme en el mundo del desarrollo web.
+
+## Habilidades
+
+- **Lenguajes de Programación:** HTML, CSS, JavaScript  
+- **Tecnologías Front-end:** React, Next.js
+- **Tecnologías Back-end:** Node.js
+- **Bases de Datos:** MongoDB
+- **Otros:** TypeScript
+
+## Proyectos Destacados
+
+Aquí encontrarás algunos de los proyectos en los que he trabajado:
+
+1. **[Spotify Clon]** - Tome el desafio con Tailwind CSS de copiar su diseño tan conocido por su elegancia y minimalismo, con una paleta de colores fresca y moderna..
+
+## Aprendizaje Continuo
+
+Estoy constantemente buscando maneras de mejorar mis habilidades y explorar nuevas tecnologías. Actualmente, estoy perfeccionando mis conocimientos en TypeScript y Next.js.
+
+## Hobbies
+
+Cuando no estoy programando, me encanta explorar el mundo de la producción musical y tocar la guitarra eléctrica. Creo que la creatividad y el aprendizaje constante van de la mano.
+
+## Contacto
+
+¡Estoy emocionado por las oportunidades de colaboración y aprendizaje! Puedes contactarme a través de [nicolasgfalabella@gmail.com] o seguirme en [LinkedIn](https://www.linkedin.com/in/nicolas-falabella-02ab47251/) para estar al tanto de mis últimos proyectos.
+
+¡Gracias por visitar mi repositorio! Espero que encuentres interesantes mis proyectos y puedas conocer un poco más sobre mí como desarrollador.
+
---
+++ components/about.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("Me");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading> Sobre mí</SectionHeading>
+      <p className="mb-3">
+        Después de obtener mi título en{" "}
+        <span className="font-medium">Administración</span>, decidí cambiar mi rumbo profesional para seguir mi pasión por la programación. Me inscribí en <a  className="underline font-bold" href="https://www.digitalhouse.com/ar" target="_blank">DigitalHouse</a>  y adquirí mi primer certificado oficial {" "}
+        <span className="font-medium">en desarrollo web </span>,{" "}
+        <span className="italic"> donde pude descubrir la satisfacción que me genera</span> la resolución de problemas y disfrutar la sensación de superar los obstáculos que se presenten. Mi stack principal 
+        es {" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, and MongoDB <br /> 
+        </span>
+        Actualmente estoy perfeccionando mi stack capacitándome en Typescript - Next.js  y estoy en búsqueda de una posición full-time donde pueda aportar mis conocimientos y mejorar mis habilidades como programador en un equipo de trabajo
+      </p>
+
+      <p>
+        <span className="italic">Cuando no estoy sumergido en código</span>, disfruto de aprender  producción musical y tocar guitarra eléctrica 🎸 para seguir mejorar mis habilidades.{" "}
+        <span className="font-medium">Mi amor por el aprendizaje autodidacta se extiende a explorar nuevas tecnologías y conceptos en mi tiempo libre, lo que me mantiene siempre motivado a la hora de aprender</span>
+      </p>
+    </motion.section>
+  );
+}
